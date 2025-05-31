210d3e96fb4d0fdd2ed869c649be36641bf049a5
Mr-Carrot888 <marip13024@gmail.com>
2025-05-31 21:32:32 +0700

    พีระมิด2ผ่าน

--- program.c
+++ program.c
@@ -1,24 +1,20 @@
-#include <stdio.h>  //พีระมิด 2 โปรแกรมนี้ยังไม่ผ่าน
+#include <stdio.h>  //พีระมิด 2
 
 int main()
 {
-int i,o;
+int i;
 scanf("%d",&i);
-o=i;
 for (int p = 1; p <= i; p++) {
 
 
-for (int c = o; c > 0; c--) {  //เว้นวรรค
+for (int c = 1; c <= i - p; c++) {  //เว้นวรรค
   printf(" ");
 }
-o-=1; // ลดค่า o ลงทีละ 1 (เพื่อให้ช่องว่างในแถวถัดไปลดลง)
 
 
-for (int c = 1; c <= p; c++){  //ปริ้น "*"
+for (int c = 1; c <= (2 * p - 1); c++){  //ปริ้น "*"
      printf("*");
   }
-p++;
-i++;
 
 
  printf("\n");
